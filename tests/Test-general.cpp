d220355ef295325a6bc09aa87504637954da5b69
Lisa Biermann <lisa.biermann@kit.edu>
2021-10-22 18:41:52 +0200

    add dummy test

---
+++ tests/Test-general.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2021 Lisa Biermann
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "catch.hpp"
+#include <TWrap/TWrap.h>
+
+TEST_CASE("test", "[general]")
+{
+    REQUIRE(true);
+}
